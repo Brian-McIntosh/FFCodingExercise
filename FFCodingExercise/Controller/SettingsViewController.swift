971c8593c19f2e2e063b80b7380d46a00ff8ddd5
Brian McIntosh <brian@mixinteractive.com>
2023-03-24 13:21:10 -0400

    Added Saving SliderValue to UserDefaults

--- FFCodingExercise/Controller/SettingsViewController.swift
+++ FFCodingExercise/Controller/SettingsViewController.swift
@@ -12,16 +12,24 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var label: UILabel!
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let savedSliderValue = defaults.integer(forKey: "SliderValue")
+        print("Saved slider value: \(savedSliderValue)")
+        slider.value = Float(savedSliderValue)
+        label.text = "\(Int(savedSliderValue)) min"
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let savedSliderValue = defaults.integer(forKey: "SliderValue")
+        print("Saved slider value: \(savedSliderValue)")
     }
     
     @IBAction func sliderValueChanged(_ sender: Any) {
         label.text = "\(Int(slider.value)) min"
+        defaults.set(slider.value, forKey: "SliderValue")
     }
-    
-    
-
 }

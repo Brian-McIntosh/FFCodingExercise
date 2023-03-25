1898ca1dffa975d5f8c47eecb06bb1b25398cfb7
Brian McIntosh <brian@mixinteractive.com>
2023-03-25 08:14:13 -0400

    Added Forecast View and Toggle Button

--- FFCodingExercise/Controller/DetailViewController.swift
+++ FFCodingExercise/Controller/DetailViewController.swift
@@ -15,17 +15,21 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var dewPointLabel: UILabel!
     @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var foreCastView: UIView!
     
     // MARK: - PROPERTIES
     var response: Response?
     var conditions: CachedConditions?
+    var isForecastShowing: Bool = false
 
     // MARK: - LIFECYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        foreCastView.isHidden = true
         // DetailVC airportAbbr: nil when view presented programmatically through viewModel as opposed to tableview
         //print("DetailVC airportAbbr: \(airportAbbr)")
+        
+        
         
         latitudeLabel.text = conditions?.lat.description
         longitudeLabel.text = conditions?.lon.description
@@ -33,4 +37,15 @@ class DetailViewController: UIViewController {
         dewPointLabel.text = conditions?.dewpointC.description
         pressureLabel.text = conditions?.pressureHg.description
     }
+    
+    @IBAction func toggleButtonPressed(_ sender: Any) {
+        if isForecastShowing == false {
+            foreCastView.isHidden = false
+            isForecastShowing = true
+        }else{
+            foreCastView.isHidden = true
+            isForecastShowing = false
+        }
+    }
+    
 }
